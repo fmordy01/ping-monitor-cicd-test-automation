5a89dd1949ea22061ab281a29ab86499900b61f9
fmordy01 <fmordych@gmail.com>
2024-11-07 21:53:47 +0000

    Adding simple logging and error handling to ping_service.py

--- ping_service.py
+++ ping_service.py
@@ -1,14 +1,29 @@
+import logging
 import requests
+
+logging.basicConfig(level=logging.INFO)
 
 def ping_host(url):
     try:
         response = requests.get(url, timeout=1)
-        return response.status_code == 200
+        
+        if response.status_code == 200:
+            logging.info(f"{url} is UP")
+            return True
+        else:
+            logging.info(f"{url} is DOWN - Status: {response.status_code}")
+            return False
+    
     except requests.ConnectionError:
+        logging.error(f"Could not connect to {url}")
+        return False
+    
+    except requests.Timeout:
+        logging.error(f"Connection to {url} timed out")
         return False
 
 if __name__ == "__main__":
-    urls = ["https://www.google.com", "https://www.cloudflare.com"]
+    urls = ["https://www.google.com", "https://www.cloudflare.com", "https://doesntexist.something.down.test.com"]
     for url in urls:
         status = ping_host(url)
-        print (f"{url} is {'up' if status else 'down'}")
+    
